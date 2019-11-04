d60c135135292ec697a096c25eae8503b1b3e6a3
Luarx <22997139+luarx@users.noreply.github.com>
2019-11-04 18:31:09 +0100

    Detect when the two Outcome Token trades with the same ID are created

--- src/LMSRMarketMakerMapping.ts
+++ src/LMSRMarketMakerMapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, log } from "@graphprotocol/graph-ts"
 import {
   LMSRMarketMaker,
   AMMCreated,
@@ -28,13 +28,39 @@ export function handleAMMFeeChanged(event: AMMFeeChanged): void {}
 export function handleAMMFeeWithdrawal(event: AMMFeeWithdrawal): void {}
 
 export function handleAMMOutcomeTokenTrade(event: AMMOutcomeTokenTrade): void {
-  // Todo change ID
-  let outcomeTokenTrade = new OutcomeTokenTrade(event.params.outcomeTokenNetCost.toHex())
-  outcomeTokenTrade.transactor = event.params.transactor
-  outcomeTokenTrade.outcomeTokenAmounts = event.params.outcomeTokenAmounts
-  outcomeTokenTrade.outcomeTokenNetCost = event.params.outcomeTokenNetCost
-  outcomeTokenTrade.marketFees = event.params.marketFees
-  outcomeTokenTrade.save()
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = OutcomeTokenTrade.load(event.transaction.hash.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity == null) {
+    entity = new OutcomeTokenTrade(event.transaction.hash.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+
+    log.info('Outcome Token trade indexed. Tx_hash: {}',
+      [
+        event.transaction.hash.toHex()
+      ]
+     )
+  } else {
+        // Show an error if there is already an AMMOutcomeTokenTrade with the same ID
+        log.error('We are trying to index a "Outcome Token trade" that is already indexed. Tx_hash: {}',
+          [
+            event.transaction.hash.toHex()
+          ]
+         )
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  entity.transactor = event.params.transactor
+  entity.outcomeTokenAmounts = event.params.outcomeTokenAmounts
+  entity.outcomeTokenNetCost = event.params.outcomeTokenNetCost
+  entity.marketFees = event.params.marketFees
+  entity.save()
 }
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
